c3bcaca5de06a0c14939e488386a9fb9154b62c1
ShalukaPiuminda <piumindajayaweera@gmail.com>
2026-06-15 15:09:33 +0530

    resolve tsconfig JSX and base config errors and formating changes

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,13 +1,11 @@
-import AppRoutes from './routes/AppRoutes'
-
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-    return (
-        <div>
-
-            <AppRoutes />
-        </div>
-    )
+  return (
+    <div>
+      <AppRoutes />
+    </div>
+  );
 }
 
-export default App
+export default App;
